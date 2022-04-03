a75ebb86e8805e7d8d58bbdb5bfe240e47cfeac4
bryceworth <bryceaworth@gmail.com>
2022-04-03 12:50:17 -0700

    commit tree 1a42c43db88a1587d756384e3bfbc3fbdd9c027a

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+const employee = {
+ Sam: {
+     key: "streetAdress"
+     
+ }
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+ const newEmployee = {...employee}
+ newEmployee[key] = value
+ return newEmployee
+}
+
+function  destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = employee
+    newEmployee[key] = value
+ return newEmployee
+
+}
+ newEmployee.value = "12 Broadway"
+
+ function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee}
+    delete newEmployee[key]
+    return newEmployee
+ }
+ 
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    // const newEmployee = employee
+    // delete newEmployee[key]
+    // return newEmployee
+    delete employee[key]
+    return employee
+}
